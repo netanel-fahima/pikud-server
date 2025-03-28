e5423f4eec9a1aba9add025cacb461a2aa6e613f
netanel-fahima <netanel.fahima@gmail.com>
2025-03-28 14:19:39 +0300

    add kikarAlerts function for fetching alerts; update sampleAlerts logging and fetch URL with cache control

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const port = 9999;
 
 const sampleAlerts = async () => {
   try {
-    const response = await fetch("https://www.kore.co.il/redAlert.json");
+    const response = await fetch(
+      "https://www.kore.co.il/redAlert.json".concat("?qc=", Date.now()),
+      {
+        cache: "no-store",
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
@@ -20,8 +25,40 @@ const sampleAlerts = async () => {
       return;
     }
 
-    console.log(`\n[${now}] 🚨🚨🚨 התקבלה התראה חדשה`);
-    console.log(data);
+    console.log(`\n[${now}] 🚨🚨🚨 new kore alert`);
+    console.log("kore", data);
+  } catch (error) {
+    const now = new Date().toLocaleTimeString("he-IL");
+    console.error(`[${now}] 🔥 שגיאה בשליפת התראה:`, error.message);
+  }
+};
+
+const kikarAlerts = async () => {
+  try {
+    const response = await fetch(
+      "https://au.kikar.co.il/v1/oref/alerts".concat(
+        "?time=",
+        Math.round(Date.now() / 2e3)
+      ),
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    const now = new Date().toLocaleTimeString("he-IL");
+
+    if (!data) {
+      // console.log(`[${now}] ❌ אין התראה`, data);
+      return;
+    }
+
+    console.log(`\n[${now}] 🚨🚨🚨 Kikar alert`);
+    console.log("kikar", data);
   } catch (error) {
     const now = new Date().toLocaleTimeString("he-IL");
     console.error(`[${now}] 🔥 שגיאה בשליפת התראה:`, error.message);
@@ -29,7 +66,8 @@ const sampleAlerts = async () => {
 };
 
 // דגימה כל שנייה
-setInterval(sampleAlerts, 500);
+setInterval(sampleAlerts, 1000);
+setInterval(kikarAlerts, 1000);
 
 // הפעלת שרת רק כדי שלא יסגר התהליך (אין צורך ב-API)
 app.listen(port, () => {
