9e9ef8703ea5292eeee33a60fd643c217ebcb088
netanel-fahima <netanel.fahima@gmail.com>
2025-10-16 14:32:38 +0300

    c

--- app.js
+++ app.js
@@ -76,10 +76,6 @@ const kikarAlerts = async () => {
   }
 };
 
-const test = async () => {
-  console.log("test");
-};
-
 // דגימה כל שנייה
 setInterval(sampleAlerts, 1000);
 setInterval(kikarAlerts, 1000);
